8a33aed2327a717845da05e213458e74fd13f0f9
Tien dat <datpt@sphoton.com>
2025-06-05 16:03:00 +0700

    fix: build android

--- babel.config.js
+++ babel.config.js
@@ -2,32 +2,32 @@ module.exports = function (api) {
   api.cache(true);
   const plugins = [
     [
-      'module-resolver',
+      "module-resolver",
       {
-        root: ['./'],
+        root: ["./"],
         alias: {
-          constants: './src/constants/',
-          routers: './src/routers/',
-          scenes: './src/scenes/',
-          utils: './src/utils/',
-          appRedux: './src/appRedux/',
-          helpers: './src/helpers/',
-          hooks: './src/hooks/',
-          services: './src/services/',
-          configs: './src/configs/',
-          components: './src/components/',
-          assets: './src/assets/',
-          model: './src/model/',
+          constants: "./src/constants/",
+          routers: "./src/routers/",
+          scenes: "./src/scenes/",
+          utils: "./src/utils/",
+          appRedux: "./src/appRedux/",
+          helpers: "./src/helpers/",
+          hooks: "./src/hooks/",
+          services: "./src/services/",
+          configs: "./src/configs/",
+          components: "./src/components/",
+          assets: "./src/assets/",
+          model: "./src/model/",
         },
       },
     ],
-    'react-native-reanimated/plugin',
+    "react-native-reanimated/plugin",
   ];
   return {
-    presets: ['module:@react-native/babel-preset'],
+    presets: ["module:@react-native/babel-preset"],
     env: {
       production: {
-        plugins: plugins.concat('transform-remove-console'),
+        plugins: plugins.concat("transform-remove-console"),
       },
       development: {
         plugins: plugins,
