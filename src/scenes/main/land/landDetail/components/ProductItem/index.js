cb1896410a5a35c037cc603136a5084ed1b8cb05
Sang <sangnhuongban2805@gmail.com>
2025-06-05 16:49:31 +0700

    fix: style land detail

--- src/scenes/main/land/landDetail/components/ProductItem/index.js
+++ src/scenes/main/land/landDetail/components/ProductItem/index.js
@@ -1,48 +1,71 @@
-import React, {useMemo, useCallback} from 'react';
+import React, { useMemo, useCallback } from 'react';
 import styles from './styles';
-import {TouchableOpacity, View} from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import AppText from 'components/AppText';
-import {PRODUCT_STATUS} from 'constants/appConstants';
+import { PRODUCT_STATUS } from 'constants/appConstants';
 import Image from '../../../../../../components/Image';
-import {IMG_APPLE, IMG_PRODUCT} from '../../../../../../assets/path';
+import { IMG_APPLE, IMG_PRODUCT } from '../../../../../../assets/path';
 import moment from "moment";
 
-function ProductItem({item, onPress, landInfo}) {
-    const {product_name, state, quantity, grow_day} = item || {};
-    const renderStatus = useMemo(() => {
-        switch (state) {
-            case PRODUCT_STATUS.SELLING:
-                return `Đang bán (${quantity})`;
-            case PRODUCT_STATUS.DONE:
-                return `Đã bán (${quantity})`;
-            case PRODUCT_STATUS.NEW:
-                return 'Ngừng kinh doanh';
-            default:
-                break;
-        }
-    }, [state, quantity]);
-    const _onPress = useCallback(() => {
-        typeof onPress === 'function' && onPress(item);
-    }, [item, onPress]);
-
-    function timeConverter(UNIX_timestamp){
-        const a = new Date(UNIX_timestamp * 1000);
-        const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-        const year = a.getFullYear();
-        const month = months[a.getMonth()];
-        const date = a.getDate();
-        const hour = a.getHours();
-        const min = a.getMinutes();
-        const sec = a.getSeconds();
-        return date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+function ProductItem({ item, onPress, landInfo }) {
+  const { product_name, state, quantity, grow_day } = item || {};
+  const renderStatus = useMemo(() => {
+    switch (state) {
+      case PRODUCT_STATUS.SELLING:
+        return `Đang bán (${quantity})`;
+      case PRODUCT_STATUS.DONE:
+        return `Đã bán (${quantity})`;
+      case PRODUCT_STATUS.NEW:
+        return 'Ngừng kinh doanh';
+      default:
+        break;
     }
+  }, [state, quantity]);
+  const _onPress = useCallback(() => {
+    typeof onPress === 'function' && onPress(item);
+  }, [item, onPress]);
 
-    return (<TouchableOpacity style={[styles.container]} onPress={_onPress}>
-        <Image source={IMG_PRODUCT} style={styles.image}/>
-            <AppText bold fit style={styles.nameText}>
-                {` ${product_name} ${landInfo?.standard_name ? '- ' + landInfo?.standard_name : ''} (${moment(timeConverter(grow_day)).format('DD/MM/YYYY')})`}
-            </AppText>
-    </TouchableOpacity>);
+  function timeConverter(UNIX_timestamp) {
+    const a = new Date(UNIX_timestamp * 1000);
+    const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    const year = a.getFullYear();
+    const month = months[a.getMonth()];
+    const date = a.getDate();
+    const hour = a.getHours();
+    const min = a.getMinutes();
+    const sec = a.getSeconds();
+    return date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+  }
+
+  return (
+    <TouchableOpacity style={[styles.container]} onPress={_onPress}>
+      <Image source={IMG_PRODUCT} style={styles.image} />
+      <AppText bold fit style={styles.nameText}>
+        {` ${product_name} ${
+          landInfo?.standard_name ? '- ' + landInfo?.standard_name : ''
+        } (${
+          grow_day
+            ? moment(timeConverter(grow_day), 'D MMM YYYY HH:mm:ss').format(
+                'DD/MM/YYYY'
+              )
+            : '--/--/----'
+        })`}
+      </AppText>
+    </TouchableOpacity>
+  );
 }
 
 export default React.memo(ProductItem);
--- src/scenes/main/land/landDetail/components/ProductItem/styles.js
+++ src/scenes/main/land/landDetail/components/ProductItem/styles.js
@@ -1,34 +1,48 @@
-import {COLOR_DARK_PRIMARY, COLOR_THIRTY, COLOR_WHITE} from 'constants/colors';
 import {
-    BORDER_RADIUS, DEFAULT_PADDING_HORIZONTAL, DEFAULT_PADDING_VERTICAL, DEVICE_WIDTH,
+  COLOR_DARK_PRIMARY,
+  COLOR_THIRTY,
+  COLOR_WHITE,
+} from 'constants/colors';
+import {
+  BORDER_RADIUS,
+  DEFAULT_PADDING_HORIZONTAL,
+  DEFAULT_PADDING_VERTICAL,
+  DEVICE_WIDTH,
 } from 'constants/size';
-import {StyleSheet} from 'react-native';
-import {scaleLandscape, scalePortrait} from 'utils/responsive';
+import { StyleSheet } from 'react-native';
+import { scaleLandscape, scalePortrait } from 'utils/responsive';
 
 export default StyleSheet.create({
-    container: {
-        width: DEVICE_WIDTH - scaleLandscape(40),
-        backgroundColor: COLOR_WHITE,
-        marginTop: scaleLandscape(16),
-        borderRadius: BORDER_RADIUS,
-        alignItems: 'flex-start',
-        padding: scaleLandscape(16),
-        marginHorizontal: scaleLandscape(20),
-    }, titleText: {
-        color: `#A5CE36`, lineHeight: scaleLandscape(21),
-        width: DEVICE_WIDTH - scaleLandscape(40),
-    }, dateText: {
-        alignSelf: 'flex-start',
-        marginTop: scaleLandscape(9),
-    }, statusText: {
-        paddingVertical: DEFAULT_PADDING_VERTICAL / 2, textDecorationLine: 'underline',
-    }, image: {
-        width: scalePortrait(60),
-        height: scalePortrait(60),
-        borderRadius: BORDER_RADIUS,
-        marginBottom: scaleLandscape(4),
-    }, nameText: {
-        color: `#0C7131`, lineHeight: scaleLandscape(21),
-        width: DEVICE_WIDTH - scaleLandscape(40),
-    }
+  container: {
+    width: DEVICE_WIDTH - scaleLandscape(40),
+    backgroundColor: COLOR_WHITE,
+    marginTop: scaleLandscape(16),
+    borderRadius: BORDER_RADIUS,
+    alignItems: 'flex-start',
+    padding: scaleLandscape(16),
+    marginHorizontal: scaleLandscape(20),
+  },
+  titleText: {
+    color: '#A5CE36',
+    lineHeight: scaleLandscape(21),
+    width: DEVICE_WIDTH - scaleLandscape(40),
+  },
+  dateText: {
+    alignSelf: 'flex-start',
+    marginTop: scaleLandscape(9),
+  },
+  statusText: {
+    paddingVertical: DEFAULT_PADDING_VERTICAL / 2,
+    textDecorationLine: 'underline',
+  },
+  image: {
+    width: scalePortrait(60),
+    height: scalePortrait(60),
+    borderRadius: BORDER_RADIUS,
+    marginBottom: scaleLandscape(4),
+  },
+  nameText: {
+    color: '#0C7131',
+    lineHeight: scaleLandscape(21),
+  },
 });
